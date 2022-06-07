46e2d6ce1af5c5a1d7b178e59b5627cacbd715fd
ematbtfw <amangealtekeba@gmail.com>
2022-06-07 10:01:47 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#include "0-putchar.h"
+#include "0-putchar.c"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char *sh = "_putchar";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
