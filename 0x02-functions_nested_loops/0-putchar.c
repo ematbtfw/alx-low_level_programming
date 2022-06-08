15947f3f2c7b0849b3a96d1607fb25eb5b66619d
ematbtfw <amangealtekeba@gmail.com>
2022-06-08 03:03:00 -0700

    putchar printed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  *Return: always 0
  */
 int main(void)
-{
+ {
         _putchar('_');
         _putchar('p');
         _putchar('u');
@@ -17,5 +17,5 @@ int main(void)
         _putchar('\n');
         
         return (0);
-
-}
+ 
+ } 
